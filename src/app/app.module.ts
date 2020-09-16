953497539c5cd5d77d82f17dcad8d4fc059db64b
missalice77 <sampaio.j.alice@gmail.com>
2020-09-16 11:27:01 +0100

    sidebar components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { LinkListComponent } from './sidebar/link-list/link-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent
+    SidebarComponent,
+    LinkListComponent
   ],
   imports: [
     BrowserModule,
